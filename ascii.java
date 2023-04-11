e65d9d7b001ecfe0cace8d547bd0e9a90acf3285
Mojtaba Esfahani <107935120+MojtabaEsfahani@users.noreply.github.com>
2023-04-11 22:25:35 +0200

    Upload The New Java Program
    How To Generate Ascii Table

---
+++ ascii.java
@@ -0,0 +1,12 @@
+
+// How To Generate Ascii Table With Java
+
+public class Main {
+    public static void main(String[] args){
+
+    for(int decimalNumber=33;decimalNumber<256;decimalNumber++){
+        char AsciiChar = (char) decimalNumber;
+        System.out.println(decimalNumber+"   is   "+AsciiChar);
+    }
+    }
+}
